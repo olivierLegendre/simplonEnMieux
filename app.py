5e6c09d8117af210b3843ca0281d6f7c860c60fa
alexishs <a.halbotschoonaert@gmail.com>
2025-06-06 13:48:55 +0200

    feat: Ajout fichier app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.orm import sessionmaker
+
+def main():
+    Base = declarative_base()
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
